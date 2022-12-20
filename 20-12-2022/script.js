c573d62a06a4d8e6962487fd1398e7e4eb478125
Luisa Zizzo <luisazizzo@MBP-di-Luisa.homenet.telecomitalia.it>
2022-12-20 16:18:26 +0100

    Esercizi del 20-12-2022

---
+++ 20-12-2022/script.js
@@ -0,0 +1,21 @@
+// Esercizio
+
+let arrayIceCream = [];
+
+for (let i = 0; i < 10; i++) {
+  arrayIceCream.push(prompt("Inserisci un gusto di gelato"));
+}
+alert("I gusti scelti da te sono: " + arrayIceCream);
+console.log("I gusti scelti da te sono: " + arrayIceCream);
+
+// Esercizio avanzato
+
+// for (let iceCream of arrayIceCream) {
+//   console.log(iceCream);
+// }
+
+// for (let iceCreamIndex in arrayIceCream) {
+//   arrayIceCream[iceCreamIndex] = prompt("Inserisci un gusto di gelato");
+// }
+
+// console.log(arrayIceCream);
