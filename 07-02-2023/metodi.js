014c512471f5f4e6736f6ad7bb5bcfc2db4c02a1
Luisazizzo <luisa0zizzo@gmail.com>
2023-02-07 15:40:43 +0100

    Esercizio del 07-02-2023

---
+++ 07-02-2023/metodi.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { user } = require("./data");
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/user", (req, res) => {
+  res.status(200).json({ sucess: true, data: user });
+});
+
+app.post("/api/user", (req, res) => {
+  const { title, body, userId } = req.body; //contenuto della richiesta post
+  if (!title || !body || !userId) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Non trovo un dato necessario" });
+  }
+  return res.status(200).json({
+    success: true,
+    user: { title: title, body: body, userId: userId },
+    user: [...user, req.body],
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server attivo alla porta ${PORT}`);
+});
